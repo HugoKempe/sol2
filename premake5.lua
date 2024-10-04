600c8071ce2bcbac106a2395ae78d8c8715b68c1
Hugo Kempe <HugoKempe04@gmail.com>
2024-10-04 02:18:08 +0200

    Fuze Engine Config

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "sol2"
+	kind "StaticLib"
+	--language "C++"
+	--staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"inclide/sol"
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
